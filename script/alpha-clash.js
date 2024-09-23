efb4048b0cc458261ee2d061348866a8f83d1ca2
Fahim-Hossain-0 <fahimhossain735@gmail.com>
2024-09-23 15:01:18 +0600

    add

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,4 +1,35 @@
 function play() {
     hideElementById('home-screen')
     showElementById('play-ground')
+    continueGame()
+}
+function continueGame() {
+    const alphabet = getARandomAlphabet()
+    const currentAlphabetElement = document.getElementById("current-alphabet")
+    currentAlphabetElement.innerText = alphabet
+    setBackgroundColorById(alphabet)
+}
+
+document.addEventListener('keyup', handelKeyboardButtonPress);
+
+function handelKeyboardButtonPress(event) {
+    const playerPress = event.key
+    const currentAlphabetElement = document.getElementById("current-alphabet")
+    const currentAlphabet = currentAlphabetElement.innerText
+    const alphabet = currentAlphabet.toLowerCase()
+    if (playerPress === alphabet) {
+        const currentScore = getTextElementValueById('current-score')
+        const updateScore = currentScore + 1
+        setTextElementValueById('current-score', updateScore)
+
+
+        removeBackgroundColorById(playerPress)
+        continueGame()
+    }
+    else {
+        const currentLife = getTextElementValueById('current-life')
+        const updateLife = currentLife - 1
+        setTextElementValueById('current-life', updateLife)
+    }
+
 }

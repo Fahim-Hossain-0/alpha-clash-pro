9fabfc08ef8c66ad45260b15002bc877379dc179
Fahim-Hossain-0 <fahimhossain735@gmail.com>
2024-09-25 16:01:15 +0600

    add

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -5,9 +5,27 @@ function play (){
 }
 function continueGame(){
     const alphabet = getARandomAlphabet() 
-    // console.log(alphabet)
-    const currentElement = getElementTextById('current-alphabet')
-    // console.log(currentElement)
-    currentElement.innerText = alphabet
-    setBackgroundColorById(alphabet)
+    const currentAlphabet = getElementTextById('current-alphabet')
+    currentAlphabet.innerText = alphabet
+    addBackgroundColorById(alphabet)
+}
+document.addEventListener('keyup',handleKeyboardButtonPress)
+function handleKeyboardButtonPress(event){
+    const playPressed = event.key
+    // console.log(playPressed)
+    const currentAlphabet = getElementTextById('current-alphabet')
+    const alphabet = currentAlphabet.innerText.toLowerCase()
+    if(playPressed === alphabet){
+        const currentScroe = getElementTextValueById('current-score')
+        const updatedScore = currentScroe + 1
+        setElementTextById('current-score',updatedScore)
+        console.log('heda')
+        removeBackgroundColorById(alphabet)
+        continueGame()
+    }
+    else{
+        
+    }
+
+
 }
--- script/utility.js
+++ script/utility.js
@@ -1,25 +1,40 @@
-function hideElementById(elementId){
+function hideElementById(elementId) {
     const element = document.getElementById(elementId)
     element.classList.add('hidden')
 }
-function showElementById(elementId){
+function showElementById(elementId) {
     const element = document.getElementById(elementId)
     element.classList.remove('hidden')
 }
-function getARandomAlphabet(){
+function getARandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
     const alphabets = alphabetString.split('')
-    const randomNumber = Math.random()*25
+    const randomNumber = Math.random() * 25
     const index = Math.round(randomNumber)
     const alphabet = alphabets[index]
-    console.log(index ,alphabet)
+    console.log(index, alphabet)
     return alphabet
 }
-function getElementTextById(elementId){
-    const element= document.getElementById(elementId)
+function getElementTextById(elementId) {
+    const element = document.getElementById(elementId)
     return element;
 }
-function setBackgroundColorById(elementId){
+function setElementTextById(elementId, value) {
+    const element = document.getElementById(elementId)
+    element.innerText = value
+}
+function getElementTextValueById(elementId){
+    const element = document.getElementById(elementId)
+    const elementText = element.innerText
+    const value = parseInt(elementText)
+    return value
+}
+
+function addBackgroundColorById(elementId) {
     const element = document.getElementById(elementId)
     element.classList.add('bg-orange-400')
+}
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-orange-400')
 }
